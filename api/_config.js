70d6abe043664b4f6c06d7ffbf2f2a2dc6202f7d
pfroment-debug <p.froment@pdj-conseil.fr>
2026-04-27 18:26:30 +0200

    Create _config.js

---
+++ api/_config.js
@@ -0,0 +1,7 @@
+// api/_config.js — Configuration Google OAuth
+export const GOOGLE = {
+  clientId: "870005971730-9m6ev329b3pkfmpqp3s81mkbm48iri46.apps.googleusercontent.com",
+  clientSecret: "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",
+  authSecret: "pdj2026crmsecretkey32characters!",
+  appUrl: "https://cr-mnotion2.vercel.app"
+};
