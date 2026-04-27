6d8e72fc5417b4dbd9031bf261e60f3a2f921cbf
pfroment-debug <p.froment@pdj-conseil.fr>
2026-04-27 19:12:43 +0200

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -19,7 +19,7 @@ const SCOPES = [
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/calendar',
   'https://www.googleapis.com/auth/calendar.events',
-  'https://www.googleapis.com/auth/gmail.readonly',
+  'https://www.googleapis.com/auth/gmail.modify',
   'https://www.googleapis.com/auth/drive.readonly'
 ].join(' ');
 
